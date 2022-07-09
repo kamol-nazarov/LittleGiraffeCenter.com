f40f0c0ac24bf4d582b0e85125655f3a590308de
kamol-nazarov <kamol.nazarov15@myhunter.cuny.edu>
2022-07-09 22:11:14 +0000

    a

--- src/pages/Home_Page/HomePageFooter.js
+++ src/pages/Home_Page/HomePageFooter.js
@@ -12,27 +12,27 @@ export default function HomePageFooter() {
             <Row className="home-page-footer-first-row" >
 
                 <Col sm={1} className="home-page-footer-about-us-link" as="a" href="/services" >
-                    <h10>ABOUT US / SERVICES</h10>
+                    <p>About Us</p>
                 </Col>
 
                 <Col sm={1} className="home-page-footer-accepted-insurance-link" as="a" href="/insurance" >
-                    <h10>ACCEPTED INSURANCE</h10>
+                    <p>Accepted Insurance</p>
                 </Col>
 
                 <Col sm={1} className="home-page-footer-payment-link" as="a" href="/makepayment" >
-                    <h10>MAKE A PAYMENT</h10>
+                    <p>Make A Payment</p>
                 </Col>
 
                 <Col sm={1} className="home-page-footer-gallery-link" as="a" href="/gallery" >
-                    <h10>PICTURE GALLERY</h10>
+                    <p>Picture Gallery</p>
                 </Col>
 
                 <Col sm={1} className="home-page-footer-classes-link" as="a" href="/classes" >
-                    <h10>CLASSES</h10>
+                    <p>Classes Offered</p>
                 </Col>
 
                 <Col sm={1} className="home-page-footer-contact-link" as="a" href="/contact" >
-                    <h10>CONTACT US</h10>
+                    <p>Contact Us</p>
                 </Col>
 
                 <Col xx-sm={1} className="home-page-footer-facebook-logo" as="a" href="http://www.facebook.com" >
